a874dd2dfd896681bd4e53ba468a9d0d61a60500
michaelslaton <michaelaaronphoto@gmail.com>
2023-08-05 12:08:34 -0700

    continued redux toolkit lessons

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -86,6 +86,11 @@ const iceCreamReducer = (state = initialIceCreamState, action) => {
         ...state,
         numOfIceCreams: state.numOfIceCreams + action.payload,
       }
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - 1,
+      }
     default:
       return state;
   };
