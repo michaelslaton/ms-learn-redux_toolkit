8ecc0a2db334274915ccfc4f8eb3d0dc972df5cf
michaelslaton <michaelaaronphoto@gmail.com>
2023-08-07 13:15:29 -0700

    Began redux react demo project setup

---
+++ react-rtk-demo/src/features/cake/CakeView.jsx
@@ -0,0 +1,9 @@
+export const CakeView = () => {
+  return (
+    <div>
+      <h2>Number of Cakes - </h2>
+      <button>Order Cake</button>
+      <button>Restock Cakes</button>
+    </div>
+  )
+}
---
+++ react-rtk-demo/src/features/icecream/IcecreamView.jsx
@@ -0,0 +1,9 @@
+export const IcecreamView = () => {
+  return (
+    <div>
+      <h2>Number of Icecreams - </h2>
+      <button>Order Icecream</button>
+      <button>Restock Icecreams</button>
+    </div>
+  )
+}
